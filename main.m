9d583f57694f793e9b0481c5ceade195d65d44ea
Munro-L <lindsaymunromail@gmail.com>
2020-01-19 19:06:02 -0500

    Fixed boundary conditions and applied axis limits for plot

--- main.m
+++ main.m
@@ -1,41 +1,46 @@
-particles = rand(1000, 4);
+num_particles = 10;
+particles = rand(num_particles, 4);
 particles(:, 2) = particles(:, 2)*200;  % x-coordinates
 particles(:, 1) = particles(:, 1)*100;  % y-coordinates
 particles(:, 3) = particles(:, 3)*10;   % placeholder x-velocity
 particles(:, 4) = particles(:, 4)*10;   % placeholder y-velocity
 
 scatter(particles(:, 2), particles(:, 1))
-xlim([0 200])
-ylim([0 100])
+axis([0 200 0 100])
 
 for i = 0:100
     % update positions
     particles(:, 1) = particles(:, 1) + particles(:, 4);
     particles(:, 2) = particles(:, 2) + particles(:, 3);
     
-    
     % check if any particles passed the boundary and deal with them
-    x_boundary_changes = particles(:, 2) > 200;
-    if any(x_boundary_changes)
-        particles(:, 2) = particles(:, 2) .* ~x_boundary_changes;
+    x_boundary_changes_right = particles(:, 2) > 200;
+    if any(x_boundary_changes_right)
+        particles(:, 2) = particles(:, 2) .* ~x_boundary_changes_right;
     end
     
-    y_boundary_changes = particles(:, 1) > 100;
-    if any(y_boundary_changes)
-        particles(:, 4) = particles(:, 4) - (2 * particles(:, 4) .* y_boundary_changes);
-        particles(:, 1) = y_boundary_changes * 100;
+    x_boundary_changes_left = particles(:, 2) < 0;
+    if any(x_boundary_changes_left)
+        particles(:, 2) = particles(:, 2) + 200 * x_boundary_changes_left - abs(particles(:, 2) .* x_boundary_changes_left);
+    end
+    
+    y_boundary_changes_upper = particles(:, 1) > 100;
+    if any(y_boundary_changes_upper)
+        particles(:, 4) = particles(:, 4) - (2 * particles(:, 4) .* y_boundary_changes_upper);
+        overshoot = (particles(:, 1) - 100) .* y_boundary_changes_upper;
+        particles(:, 1) = particles(:, 1) - 2 * overshoot;
     end
     
     y_boundary_changes_lower = particles(:, 1) < 0;
     if any(y_boundary_changes_lower)
         particles(:, 4) = particles(:, 4) - (2 * particles(:, 4) .* y_boundary_changes_lower);
-        particles(:, 1) = particles(:, 1) .* ~y_boundary_changes_lower;
+        overshoot = abs(particles(:, 1)) .* y_boundary_changes_lower;
+        particles(:, 1) = particles(:, 1) + 2 * overshoot;
     end
     
-    
+    % refresh the plot
     scatter(particles(:, 2), particles(:, 1))
-    xlim([0 200])
-    ylim([0 100])
+    axis([0 200 0 100])
     refreshdata
     drawnow
 end
