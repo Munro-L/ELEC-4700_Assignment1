104aaa513dc1b4a161347995da6d8221063de6ba
Munro-L <lindsaymunromail@gmail.com>
2020-01-27 15:29:06 -0500

    Fixed plot for particles that pass x-boundary, and added normal distribution for thermal velocity

--- main.m
+++ main.m
@@ -1,15 +1,19 @@
+% adjustable constants
 num_particles = 7;
 colours = ["r", "g", "b", "c", "y", "k", "m"];
-particles = rand(num_particles, 4);
+vth = 1.0;      % placeholder value until I know what I'm doing
+
+% particle positions
+particles = rand(num_particles, 2);
 particles(:, 2) = particles(:, 2)*200;  % x-coordinates
 particles(:, 1) = particles(:, 1)*100;  % y-coordinates
-particles(:, 3) = particles(:, 3)*10;   % placeholder x-velocity
-particles(:, 4) = particles(:, 4)*10;   % placeholder y-velocity
 
-%scatter(particles(:, 2), particles(:, 1))
-%axis([0 200 0 100])
+% normal distribution shifted by themal velocity
+% column 3 is x-velocity, 4 is x-velocity
+particles(:, 3:4) = randn(num_particles, 2) + vth;    
 
-for i = 0:100
+% main time loop
+for i = 0:1000
     previous_particles = particles;
     
     % update positions
@@ -21,7 +25,7 @@ for i = 0:100
     if any(x_boundary_changes_right)
         particles(:, 2) = particles(:, 2) .* ~x_boundary_changes_right;
     end
-    
+
     x_boundary_changes_left = particles(:, 2) < 0;
     if any(x_boundary_changes_left)
         particles(:, 2) = particles(:, 2) + 200 * x_boundary_changes_left - abs(particles(:, 2) .* x_boundary_changes_left);
@@ -41,15 +45,16 @@ for i = 0:100
         particles(:, 1) = particles(:, 1) + 2 * overshoot;
     end
     
-    % refresh the plot
-    %scatter(particles(:, 2), particles(:, 1))
+    % plot position updates of particles, 
+    % ignoring those that passed the x-boundary
+    x_boundary_affected = x_boundary_changes_right | x_boundary_changes_left;
     for i = 1:length(particles)
-        plot([previous_particles(i, 2) particles(i, 2)], [previous_particles(i, 1) particles(i, 1)], colours(i))
+        if ~x_boundary_affected(i)
+            plot([previous_particles(i, 2) particles(i, 2)], [previous_particles(i, 1) particles(i, 1)], colours(i))
+        end
     end
     axis([0 200 0 100])
     hold on
-    pause(1)
-    %refreshdata
-    %drawnow
+    pause(0.1)
 end
 
