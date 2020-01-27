0c5cec74b8386fd39df821a31c0aa3bb8becbae5
Munro-L <lindsaymunromail@gmail.com>
2020-01-27 16:06:42 -0500

    fixd colour indexing to support more particles

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 % adjustable constants
-num_particles = 7;
+num_particles = 100;
 colours = ["r", "g", "b", "c", "y", "k", "m"];
 vth = 1.0;      % placeholder value until I know what I'm doing
-mean_time_collision = 0.2   % measured in picoseconds
-timesteps = 1E-3            % also in picoseconds, so this will be 1 femto second timesteps
+mean_time_collision = 0.2;   % measured in picoseconds
+timesteps = 1E-3;            % also in picoseconds, so this will be 1 femto second timesteps
 pscat = 1 - exp(-timesteps/mean_time_collision);
 
 % particle positions
@@ -14,6 +14,8 @@ particles(:, 1) = particles(:, 1)*100;  % y-coordinates
 % normal distribution shifted by themal velocity
 % column 3 is x-velocity, 4 is x-velocity
 particles(:, 3:4) = randn(num_particles, 2) + vth;    
+histogram(particles(:, 3:4), 10);
+
 
 % main time loop
 for i = 0:1000      % each step is a femtosecond
@@ -61,7 +63,7 @@ for i = 0:1000      % each step is a femtosecond
     x_boundary_affected = x_boundary_changes_right | x_boundary_changes_left;
     for i = 1:length(particles)
         if ~x_boundary_affected(i)
-            plot([previous_particles(i, 2) particles(i, 2)], [previous_particles(i, 1) particles(i, 1)], colours(i))
+            plot([previous_particles(i, 2) particles(i, 2)], [previous_particles(i, 1) particles(i, 1)], colours(mod(i, length(colours)) + 1))
         end
     end
     axis([0 200 0 100])
